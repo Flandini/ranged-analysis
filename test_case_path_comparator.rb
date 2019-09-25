bc52896d6366bdcf52a9fa354a66a5bec8982fe7
michael <michael@michael>
2019-09-25 16:29:20 -0500

    Add code to randomly select test cases for static range refinement

--- test_case_path_comparator.rb
+++ test_case_path_comparator.rb
@@ -53,7 +53,44 @@ def compare(directory)
   puts pathFiles
 
   puts "Sorted paths are: "
-  puts pathFiles.map { |fileName| TestCase.new(fileName) }.sort { |a, b| a <=> b }.map(&:name)
+  sortedPaths = pathFiles.map { |fileName| TestCase.new(fileName) }.sort { |a, b| a <=> b }.map(&:name)
+  puts sortedPaths
+  sortedPaths
+
+  static_range_select(sortedPaths)
+end
+
+def static_range_select(pathFiles)
+  if pathFiles.length <= 9
+    puts "Not enough test cases"
+    return
+  end
+
+  # Resort but according to string order
+  lastTestCaseGenerated = get_last_test_case(pathFiles)
+  startTestCase = pathFiles.first
+
+  lastTestCaseIndex = pathFiles.find_index(lastTestCaseGenerated)
+  testCasesLessThanLast = pathFiles[0..lastTestCaseIndex]
+
+  numTestCasesToTake = 9
+  chosenTestCases = []
+  chosenTestCases << startTestCase
+  chosenTestCases << lastTestCaseGenerated
+
+  while chosenTestCases.length != numTestCasesToTake
+    chosenTestCases << testCasesLessThanLast.sample
+    chosenTestCases = chosenTestCases.uniq
+  end
+
+  sorted = chosenTestCases.map { |fileName| TestCase.new(fileName) }.sort { |a, b| a <=> b }.map(&:name)
+  puts "Chose test cases:"
+  puts sorted
+  sorted
+end
+
+def get_last_test_case(pathFiles)
+  pathFiles.sort[-1]
 end
 
 compare ARGV[0]
